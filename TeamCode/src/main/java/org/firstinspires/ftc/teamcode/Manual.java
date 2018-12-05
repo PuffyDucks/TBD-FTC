c471a9772943090d56dbf05300770d2dcf49398f
cool9128 <9128cool@gmail.com>
2018-12-04 17:33:18 -0800

    Autonomous Mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manual.java
@@ -26,99 +26,99 @@ public class Manual extends LinearOpMode {
 
   @Override
   public void runOpMode() {
-      LeftWheel = hardwareMap.dcMotor.get("Left Wheel");
-      RightWheel = hardwareMap.dcMotor.get("Right Wheel");
-      DefaultArm = hardwareMap.dcMotor.get("Default Arm");
-      LeftClaw = hardwareMap.servo.get("Left Claw");
-      RightClaw = hardwareMap.servo.get("Right Claw");
-      InceptionArm = hardwareMap.dcMotor.get("Inception Arm");
-      call();
-      set();
-      while (opModeIsActive()) {
-          setVariables();
-          defaultArm();
-          inceptionArm();
-          move();
-          print();
-      }
+    LeftWheel = hardwareMap.dcMotor.get("Left Wheel");
+    RightWheel = hardwareMap.dcMotor.get("Right Wheel");
+    DefaultArm = hardwareMap.dcMotor.get("Default Arm");
+    LeftClaw = hardwareMap.servo.get("Left Claw");
+    RightClaw = hardwareMap.servo.get("Right Claw");
+    InceptionArm = hardwareMap.dcMotor.get("Inception Arm");
+    call();
+    set();
+    while (opModeIsActive()) {
+      setVariables();
+      defaultArm();
+      inceptionArm();
+      move();
+      print();
+    }
   }
 
   private void call() {
-      // Call program, and devices
-      waitForStart();
-      ((DcMotorEx) DefaultArm).setMotorEnable();
-      ((DcMotorEx) LeftWheel).setMotorEnable();
-      ((DcMotorEx) RightWheel).setMotorEnable();
+    // Call program, and devices
+    waitForStart();
+    ((DcMotorEx) DefaultArm).setMotorEnable();
+    ((DcMotorEx) LeftWheel).setMotorEnable();
+    ((DcMotorEx) RightWheel).setMotorEnable();
   }
 
   private void set() {
-      // Set direction of devices
-      DefaultArm.setDirection(DcMotorSimple.Direction.FORWARD);
-      LeftClaw.setDirection(Servo.Direction.REVERSE);
-      RightClaw.setDirection(Servo.Direction.FORWARD);
-      InceptionArm.setDirection(DcMotorSimple.Direction.FORWARD);
-      LeftWheel.setDirection(DcMotorSimple.Direction.FORWARD);
-      RightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+    // Set direction of devices
+    DefaultArm.setDirection(DcMotorSimple.Direction.FORWARD);
+    LeftClaw.setDirection(Servo.Direction.REVERSE);
+    RightClaw.setDirection(Servo.Direction.FORWARD);
+    InceptionArm.setDirection(DcMotorSimple.Direction.FORWARD);
+    LeftWheel.setDirection(DcMotorSimple.Direction.FORWARD);
+    RightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
   }
 
   private void setVariables() {
-      // Sets turbo mode with variables
-      // Right bumper input
-      if (gamepad1.right_bumper) {
-          wheelSpeed = 5;
-          armSpeed = 5;
-      } else {
-          wheelSpeed = 1;
-          armSpeed = 2;
-      }
+    // Sets turbo mode with variables
+    // Right bumper input
+    if (gamepad1.right_bumper) {
+      wheelSpeed = 5;
+      armSpeed = 5;
+    } else {
+      wheelSpeed = 1;
+      armSpeed = 2;
+    }
   }
 
   private void defaultArm() {
-      // Movement of default arm
-      // Right bumper, left bumper, Y and X button inputs
-      if (gamepad1.right_bumper && gamepad1.left_bumper) {
-          DefaultArm.setPower(-0.1);
-      } else if (gamepad1.left_bumper) {
-          LeftClaw.setPosition(0.8);
-          RightClaw.setPosition(0.8);
-      } else if (gamepad1.y) {
-          DefaultArm.setPower(0.25 * armSpeed);
-      } else if (gamepad1.x) {
-          DefaultArm.setPower(-0.25 * armSpeed);
-      } else {
-          LeftClaw.setPosition(0.3);
-          RightClaw.setPosition(0.3);
-          DefaultArm.setPower(0);
-      }
+    // Movement of default arm
+    // Right bumper, left bumper, Y and X button inputs
+    if (gamepad1.right_bumper && gamepad1.left_bumper) {
+      DefaultArm.setPower(-0.1);
+    } else if (gamepad1.left_bumper) {
+      LeftClaw.setPosition(0.8);
+      RightClaw.setPosition(0.8);
+    } else if (gamepad1.y) {
+      DefaultArm.setPower(0.25 * armSpeed);
+    } else if (gamepad1.x) {
+      DefaultArm.setPower(-0.25 * armSpeed);
+    } else {
+      LeftClaw.setPosition(0.3);
+      RightClaw.setPosition(0.3);
+      DefaultArm.setPower(0);
+    }
   }
 
   private void inceptionArm() {
-      // Movement of inception arm
-      // B and A button inputs
-      if (gamepad1.b) {
-          InceptionArm.setPower(0.3);
-      } else if (gamepad1.a) {
-          InceptionArm.setPower(-0.3);
-      } else {
-          InceptionArm.setPower(0);
-      }
+    // Movement of inception arm
+    // B and A button inputs
+    if (gamepad1.b) {
+      InceptionArm.setPower(0.3);
+    } else if (gamepad1.a) {
+      InceptionArm.setPower(-0.3);
+    } else {
+      InceptionArm.setPower(0);
+    }
   }
 
   private void move() {
-      // Movement of robot with wheels
-      // Left analog stick input
-      LeftWheel.setPower(gamepad1.left_stick_y * 0.5 * wheelSpeed - gamepad1.left_stick_x * 0.5 * wheelSpeed);
-      RightWheel.setPower(gamepad1.left_stick_y * 0.5 * wheelSpeed + gamepad1.left_stick_x * 0.5 * wheelSpeed);
+    // Movement of robot with wheels
+    // Left analog stick input
+    LeftWheel.setPower(gamepad1.left_stick_y * 0.5 * wheelSpeed - gamepad1.left_stick_x * 0.5 * wheelSpeed);
+    RightWheel.setPower(gamepad1.left_stick_y * 0.5 * wheelSpeed + gamepad1.left_stick_x * 0.5 * wheelSpeed);
   }
 
   private void print() {
-      // Prints data for debug purposes
-      telemetry.addData("joystick", gamepad1.left_stick_x);
-      telemetry.addData("Left Wheel Power", LeftWheel.getPower());
-      telemetry.addData("Right Wheel Power", RightWheel.getPower());
-      telemetry.addData("Left Wheel Position", LeftWheel.getCurrentPosition());
-      telemetry.addData("Right Wheel Position", RightWheel.getCurrentPosition());
-      telemetry.addData("Arm Position", DefaultArm.getCurrentPosition());
-      telemetry.update();
+    // Prints data for debug purposes
+    telemetry.addData("joystick", gamepad1.left_stick_x);
+    telemetry.addData("Left Wheel Power", LeftWheel.getPower());
+    telemetry.addData("Right Wheel Power", RightWheel.getPower());
+    telemetry.addData("Left Wheel Position", LeftWheel.getCurrentPosition());
+    telemetry.addData("Right Wheel Position", RightWheel.getCurrentPosition());
+    telemetry.addData("Arm Position", DefaultArm.getCurrentPosition());
+    telemetry.update();
   }
 }
