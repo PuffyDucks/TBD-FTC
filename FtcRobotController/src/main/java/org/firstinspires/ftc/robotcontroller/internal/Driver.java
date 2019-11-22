afa5742a3fcfa45ff5022ced2a7529aea5e28035
yoshilarkins <yoshilarkins@gmail.com>
2019-11-21 17:13:37 -0800

    Merge branch 'master' of /Users/Andrew/Desktop/TBD-FTC with conflicts.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Driver.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Driver.java
@@ -16,24 +16,43 @@ public class Driver extends LinearOpMode{
   private DcMotor ClawArm;
   private DcMotor ClawArm2;
   private DcMotor ClawArm3;
+  private Servo servo;
+  private Servo servo2;
 
-  double leftFront = 1;
-  double rightFront = 1;
-  double leftBack = 1;
-  double rightBack = 1;
+double leftFront = 1;
+double rightFront = 1;
+double leftBack = 1;
+double rightBack = 1;
+
+  private double multiplier = 1;
+
   /**
   * This function is executed when this Op Mode is selected from the Driver Station.
   */
 
   @Override
   public void runOpMode() {
+    //map motors and servos
     LeftFront = hardwareMap.dcMotor.get("LeftFront");
     LeftBack = hardwareMap.dcMotor.get("LeftBack");
     RightFront = hardwareMap.dcMotor.get("RightFront");
     RightBack = hardwareMap.dcMotor.get("RightBack");
     ClawArm = hardwareMap.dcMotor.get("ClawArm");
     ClawArm2 = hardwareMap.dcMotor.get("ClawArm2");
-    ClawArm3 = hardwareMap.dcMotor.get("ClawArm3");
+    servo = hardwareMap.get(Servo.class, "Grab");
+    servo2 = hardwareMap.get(Servo.class, "Grab2");
+
+//    //set encoder modes
+//    LeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//    LeftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//    RightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//    RightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//
+//    LeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//    LeftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//    RightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//    RightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
 
     //these are functions below
     call();
@@ -54,8 +73,8 @@ public class Driver extends LinearOpMode{
     ((DcMotorEx) LeftBack).setMotorEnable();
     ((DcMotorEx) RightFront).setMotorEnable();
     ((DcMotorEx) RightBack).setMotorEnable();
-    // ((DcMotorEx) ClawArm).setMotorEnable();
-    // ((DcMotorEx) ClawArm2).setMotorEnable();
+    ((DcMotorEx) ClawArm).setMotorEnable();
+    ((DcMotorEx) ClawArm2).setMotorEnable();
   }
 
   private void set() {
@@ -64,28 +83,36 @@ public class Driver extends LinearOpMode{
     LeftBack.setDirection(DcMotorSimple.Direction.FORWARD);
     RightFront.setDirection(DcMotorSimple.Direction.REVERSE);
     RightBack.setDirection(DcMotorSimple.Direction.REVERSE);
-    // ClawArm.setDirection(DcMotorSimple.Direction.FORWARD);
-    // ClawArm2.setDirection(DcMotorSimple.Direction.FORWARD);
+    ClawArm.setDirection(DcMotorSimple.Direction.FORWARD);
+    ClawArm2.setDirection(DcMotorSimple.Direction.FORWARD);
   }
 
   private void move() {
     // Movement of robot with wheels
     // Left analog stick input
-    LeftFront.setPower(((gamepad1.left_stick_y - gamepad1.left_stick_x) * 0.5 + gamepad1.right_stick_x * 0.3) * leftFront);
-    LeftBack.setPower(((gamepad1.left_stick_y + gamepad1.left_stick_x) * 0.5 + gamepad1.right_stick_x * 0.3) * rightFront);
-    RightFront.setPower(((gamepad1.left_stick_y + gamepad1.left_stick_x) * 0.5 - gamepad1.right_stick_x * 0.3) * leftBack);
-    RightBack.setPower(((gamepad1.left_stick_y - gamepad1.left_stick_x) * 0.5 - gamepad1.right_stick_x * 0.3) * rightBack);
+    LeftFront.setPower(((gamepad1.left_stick_y - gamepad1.left_stick_x) * 0.5 + gamepad1.right_stick_x * 0.3) * leftFront * multiplier);
+    LeftBack.setPower(((gamepad1.left_stick_y + gamepad1.left_stick_x) * 0.5 + gamepad1.right_stick_x * 0.3) * rightFront * multiplier);
+    RightFront.setPower(((gamepad1.left_stick_y + gamepad1.left_stick_x) * 0.5 - gamepad1.right_stick_x * 0.3) * leftBack * multiplier);
+    RightBack.setPower(((gamepad1.left_stick_y - gamepad1.left_stick_x) * 0.5 - gamepad1.right_stick_x * 0.3) * rightBack * multiplier);
 
+    //multiplier for arm strength
+    if(gamepad1.right_trigger>0.5) {
+      multiplier = 2;
+    } else {
+      multiplier = 1;
+    }
+    //lift control
     if(gamepad1.a) {
-      ClawArm.setPower(0.3);
+      ClawArm.setPower(0.6*multiplier);
     }
     else if(gamepad1.b) {
-      ClawArm.setPower(-0.3);
+      ClawArm.setPower(-0.6*multiplier);
     }
     else {
       ClawArm.setPower(0);
     }
 
+    //controls whole arm movement up and down
     if(gamepad1.x) {
       ClawArm2.setPower(0.3);
     }
@@ -93,38 +120,39 @@ public class Driver extends LinearOpMode{
       ClawArm2.setPower(-0.3);
     }
     else {
-      ClawArm2.setPower(0);
+      ClawArm2.setPower(-0.05);
     }
-  }
-
-  private void callibrate() {
-    if(gamepad2.a) {
-      leftBack =+ 0.001 * gamepad2.left_stick_x;
-    }
-    if(gamepad2.b) {
-      rightBack =+ 0.001 * gamepad2.left_stick_x;
-    }
-    if(gamepad2.x) {
-      rightFront =+ 0.001 * gamepad2.left_stick_x;
-    }
-    if(gamepad2.y) {
-      leftFront =+ 0.001 * gamepad2.left_stick_x;
+    if(gamepad1.left_trigger > 0.5){
+        servo.setPosition(0.5);
+        servo2.setPosition(0.3);
+    } else {
+      servo.setPosition(0);
+      servo2.setPosition(0.8);
     }
   }
 
   private void print() {
     // Prints data for debug purposes
-    telemetry.addData("leftFront", leftFront);
-    telemetry.addData("rightFront", rightFront);
-    telemetry.addData("leftBack", leftBack);
-    telemetry.addData("rightBack", rightBack);
-    // telemetry.addData("Left Wheel Power", LeftWheel.getPower());
-    // telemetry.addData("Right Wheel Power", RightWheel.getPower());
-    //telemetry.addData("Left Wheel Position", LeftWheel.getCurrentPosition());
-    //telemetry.addData("Right Wheel Position", RightWheel.getCurrentPosition());
-    //telemetry.addData("GamePad2 Left Stick Y", gamepad2.left_stick_y);
-    //telemetry.addData("Claw 1 Power", ClawArm.getPower());
-    //telemetry.addData("Claw 2 Power", ClawArm2.getPower());
+    telemetry.addData("Encode LB", LeftBack.getCurrentPosition());
+    telemetry.addData("Encode LF", LeftFront.getCurrentPosition());
+    telemetry.addData("Encode RF", RightFront.getCurrentPosition());
+    telemetry.addData("Encode RB", RightBack.getCurrentPosition());
+
+    telemetry.addData("LB", LeftBack.getPower());
+    telemetry.addData("RB", RightBack.getPower());
+    telemetry.addData("LF", LeftFront.getPower());
+    telemetry.addData("RF", RightFront.getPower());
+    telemetry.addData("leftFront", rightFront);
+    telemetry.addData("rightBack", leftBack);
+    telemetry.addData("rightFront", rightBack);
+    telemetry.addData("leftBack", leftFront);
+//     telemetry.addData("Left Wheel Power", LeftWheel.getPower());
+//     telemetry.addData("Right Wheel Power", RightWheel.getPower());
+//    telemetry.addData("Left Wheel Position", LeftWheel.getCurrentPosition());
+//    telemetry.addData("Right Wheel Position", RightWheel.getCurrentPosition());
+//    telemetry.addData("GamePad2 Left Stick Y", gamepad2.left_stick_y);
+//    telemetry.addData("Claw 1 Power", ClawArm.getPower());
+//    telemetry.addData("Claw 2 Power", ClawArm2.getPower());
     telemetry.update();
   }
 }
